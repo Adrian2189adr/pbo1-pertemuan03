5e99dc1f8f3ff7021a88a7a983c5afdcd59c4015
adrian2189 <adrianfadilah2189@gmail.com>
2024-05-05 16:16:01 +0700

    True Statement1

---
+++ src/IF_ELSE/Percabangan.java
@@ -0,0 +1,14 @@
+package IF_ELSE;
+
+public class Percabangan {
+    public static void main(String[] args) {
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
+
+        if (uangDiDompet<totalBelanja){
+            System.out.println("Uang kurang, kerja woi");
+        }else {
+            System.out.println("Uang cukup, K");
+        }
+    }
+}
