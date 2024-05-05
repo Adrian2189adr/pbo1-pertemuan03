1f1f0429946343e799aa44ed088b13a07a7f21e3
adrian2189 <adrianfadilah2189@gmail.com>
2024-05-05 16:33:43 +0700

    Percabangan Switch

---
+++ src/Switch/Percabangan_Switch.java
@@ -0,0 +1,24 @@
+package Switch;
+
+public class Percabangan_Switch {
+    public static void main(String[] args) {
+        int jalanjalan=1;
+        switch (jalanjalan){
+            case 1:
+                System.out.println("Japan");
+                break;
+            case 2:
+                System.out.println("Germany");
+                break;
+            case 3:
+                System.out.println("South Korea");
+                break;
+            case 4:
+                System.out.println("Russia");
+                break;
+            default:
+                System.out.println("Banjar");
+                break;
+        }
+    }
+}
