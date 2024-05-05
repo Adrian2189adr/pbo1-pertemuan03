fbedf52159355e6174ce6a414baf36694ae05b35
adrian2189 <adrianfadilah2189@gmail.com>
2024-05-05 16:24:42 +0700

    Percabangan IF ELSE (Statement3)

--- src/IF_ELSE/Percabangan.java
+++ src/IF_ELSE/Percabangan.java
@@ -2,13 +2,16 @@ package IF_ELSE;
 
 public class Percabangan {
     public static void main(String[] args) {
-        double totalBelanja = 25000;
+        double totalBelanja = 53000;
         double uangDiDompet = 53000;
 
         if (uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kerja woi");
+        }else if (uangDiDompet>totalBelanja){
+            double angsul = uangDiDompet-totalBelanja;
+            System.out.println("Uang cukup, kembalian :" +angsul);
         }else {
-            System.out.println("Uang cukup, K");
+            System.out.println("Uang pas.. njaluk ceban , gua ganti entar...");
         }
     }
 }
